abd597d5e7f4c2074a78624279c3e169154b91d1
Priyanshu Bangwal <priyanshubangwal31@gmail.com>
2026-08-13 16:59:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (98.51%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,34 +1,28 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<int> st;
-        int n= s.size();
-        if(n==1){
-            return false;
-        }
-        int i=n-1;
+        stack<char>st;
+        bool k= true;
+        int i=s.length()-1;
         while(i>=0){
             if(s[i]==')' || s[i]==']' || s[i]=='}'){
                 st.push(s[i]);
             }else{
-                if(!st.empty() && (s[i]=='(' && st.top()==')')){
-                    st.pop();
-                }else if(!st.empty() && (s[i]=='[' && st.top()==']')){
-                    st.pop();
-                }else if(!st.empty() && (s[i]=='{' && st.top()=='}')){
+                if(!st.empty() && s[i]=='(' && st.top()==')' || !st.empty() && s[i]=='[' && st.top()==']' || !st.empty() && s[i]=='{' && st.top()=='}') {
                     st.pop();
                 }else{
-                    break;
+                   k=false;
+                   break;
                 }
+                
             }
             i--;
         }
-
-        if(i<0 && st.empty()){
-            return true;
+        
+        if(!st.empty() || i>0){
+            k=false;
         }
-
-        return false;
-
+        
+        return k;
     }
 };
